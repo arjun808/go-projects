3ee1de776429d985cbbce8cb77e68dd06b609ead
root <root@ip-172-31-17-30.us-west-1.compute.internal>
2018-04-06 07:40:46 +0000

    Tweaked the instance numbering style

--- instanceCreation.go
+++ instanceCreation.go
@@ -32,14 +32,14 @@ Imagestring := r.FormValue("image")
 Countint, _ := strconv.ParseInt(r.FormValue("inscount"), 0, 64)
 i, _ := strconv.ParseInt("0", 0, 64)
 for Countint > i {
-   Nodestringfinal := fmt.Sprintf("%s-%d", Nodestring, i)
-   output := CreateInstance(Nodestringfinal, Imagestring, Countint, i)
+   Nodestringfinal := fmt.Sprintf("%s-%d", Nodestring, i+1)
+   output := CreateInstance(Nodestringfinal, Imagestring, Countint)
    i++
    fmt.Fprintf(w, output)
               }
 }
 
-func CreateInstance(Nodename string, Image string, Inscount int64, i int64) (string) {
+func CreateInstance(Nodename string, Image string, Inscount int64) (string) {
 data := "#!/bin/bash \n ip=$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1) \n echo $ip > /tmp/ip.txt \n sed -i s/App4.3/"+ Nodename  +"/g /etc/consul.d/agent.json \n sed -i s/127.0.0.1/$ip/g /etc/consul.d/agent.json \n sed -i s/127.0.0.1/$ip/g /etc/consul.d/apache.json \n sed -i s/3/$ip/g /var/www/html/index.html \n consul agent -config-dir /etc/consul.d/"
 sEnc := b64.StdEncoding.EncodeToString([]byte(data))
 
@@ -71,6 +71,7 @@ svc := ec2.New(session.New(&aws.Config{Region: aws.String("us-west-1")}))
   if err == nil {
     msg = resp
     }
+	
   json_val, _ := json.MarshalIndent(msg, "", " ")
   return string(json_val)
 }
